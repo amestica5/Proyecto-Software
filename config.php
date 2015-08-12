8c44882517037c9d77fb4ace604803f74e609499
amestica5 <martin.amestica@gmail.com>
2015-08-12 20:19:16 -0300

    dwd

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$mysql_host ="http://54.68.135.137/";
+$mysql_user ="root";
+$mysql_password="root";
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+include(config.php);
+$link = mysql_connect($mysql_host, $mysql_user, $mysql_password)
+    or die('No se pudo conectar: ' . mysql_error());
+echo 'Connected successfully';
+mysql_select_db('my_database') or die('No se pudo seleccionar la base de datos');
+?>
